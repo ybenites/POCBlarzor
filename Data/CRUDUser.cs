20139dfb713756697aea0e4359a976da8fbfcb47
Yime Benites <benites_alf@hotmail.com>
2021-03-18 14:37:03 -0500

    POC blazor consumer

---
+++ Data/CRUDUser.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+using System;
+
+
+namespace BlazorApp.Data
+{
+    public class CRUDUser
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("job")]
+        public string Job { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedTimestamp { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime UpdatedTimestamp { get; set; }
+    }
+}
---
+++ Data/User.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+using System;
+
+
+namespace BlazorApp.Data
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("avatar")]
+        public string AvatarURI { get; set; }
+    }
+}
